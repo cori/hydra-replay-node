acb6563dd7aa129f94f751fc9b5840694fc807d5
Glitch (hello-webpage) <none>
2021-04-10 20:40:22 +0000

    🐑🤑 Checkpoint ./index.js:9838890/125 ./package.json:9838890/60 ./public/index.html:9838890/946 ./app/index.js:9838890/10539 ./app/record.js:9838890/3513

--- index.js
+++ index.js
@@ -6,3 +6,8 @@ const server = require("http").createServer(app);
 const io = require("socket.io")(server);
 const port = process.env.PORT || 3000;
 
+server.listen(port, function() {
+  console.log("Server listening at port %d", port);
+});
+
+app.use(express.static("public"));
